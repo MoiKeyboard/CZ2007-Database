0e10b1205438fc4e87335dc39c7536d3f91356d9
WeiMin <weimin.mok@gmail.com>
2020-03-24 22:57:19 +0800

    Remove uniques for key and change CreditCard to CustomerID in Order

--- Creation/CreateAll.sql
+++ Creation/CreateAll.sql
@@ -10,7 +10,6 @@ CREATE TABLE Customer(
     UserName VARCHAR(20) NOT NULL,
     UserPass VARCHAR(20) NOT NULL,
     
-	UNIQUE(CustomerID),
 	UNIQUE(Email),
 	UNIQUE(UserName),
 
@@ -22,7 +21,6 @@ CREATE TABLE CreditCard (
 	Expiry DATE	NOT NULL,
     CustomerID INT NOT NULL,
 
-	UNIQUE(CreditCardNumber),
     PRIMARY KEY (CreditCardNumber),
     FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID)
 );
@@ -32,7 +30,6 @@ CREATE TABLE ProductType(
     ProductTypeDesc VARCHAR(500),
     ParentID INT,
     
-	UNIQUE(ProductTypeID),
     PRIMARY KEY (ProductTypeID),
     FOREIGN KEY (ParentID) REFERENCES ProductType(ProductTypeID)
 );
@@ -46,7 +43,6 @@ CREATE TABLE Product (
     Colour VARCHAR(20),
     Size VARCHAR(20),
 
-	UNIQUE(ProductID),
     PRIMARY KEY (ProductID),
     FOREIGN KEY (ProductTypeID) REFERENCES ProductType(ProductTypeID)
 );
@@ -56,9 +52,8 @@ CREATE TABLE Photo (
     Photo VARCHAR(20) NOT NULL,
     ProductID INT NOT NULL
     
-
-  UNIQUE (PhotoID),
-    PRIMARY KEY (PhotoID, Photo),
+	UNIQUE(Photo),
+    PRIMARY KEY (PhotoID),
 
     FOREIGN KEY (ProductID) REFERENCES Product(ProductID)
     ON DELETE CASCADE ON UPDATE CASCADE,
@@ -76,7 +71,7 @@ CREATE TABLE RestrictedShop (
     ShopID INT NOT NULL,
     ProductTypeID INT NOT NULL,
 
-    PRIMARY KEY (ShopID, ProductTypeID)
+    PRIMARY KEY (ShopID, ProductTypeID),
     FOREIGN KEY (ShopID) REFERENCES Shop(ShopID),
     FOREIGN KEY (ProductTypeID) REFERENCES ProductType(ProductTypeID)
 
@@ -87,7 +82,6 @@ CREATE TABLE Invoice (
     InvoiceDate DATE NOT NULL,
     InvoiceStatus VARCHAR(20) NOT NULL,
 
-    UNIQUE (InvoiceNumber),
     PRIMARY KEY (InvoiceNumber)
 );
 
@@ -96,7 +90,6 @@ CREATE TABLE Payment (
     PaymentDate DATE NOT NULL,
     Amount DECIMAL(8,2) NOT NULL,
 
-	UNIQUE (PaymentID),
     PRIMARY KEY (PaymentID)
 );
 
@@ -104,11 +97,11 @@ CREATE TABLE Orders (
     OrderID INT NOT NULL IDENTITY(1,1),
     OrderDate DATE NOT NULL,
     OrderStatus VARCHAR(20) NOT NULL,
-    CreditCardNumber VARCHAR(20) NOT NULL,
+    CustomerID INT NOT NULL,
     InvoiceNumber INT NOT NULL,
 
     PRIMARY KEY (OrderID),
-    FOREIGN KEY (CreditCardNumber) REFERENCES CreditCard(CreditCardNumber),
+    FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID),
     FOREIGN KEY (InvoiceNumber) REFERENCES Invoice(InvoiceNumber)
 );
 
@@ -117,7 +110,6 @@ CREATE TABLE Shipment (
     TrackingNo INT,
     ShipmentDate DATE,
 	
-	UNIQUE (ShipmentID),
     PRIMARY KEY (ShipmentID)
 );
 
@@ -130,7 +122,6 @@ CREATE TABLE OrderItem (
     UnitPrice DECIMAL(8,2) NOT NULL,
     ItemStatus VARCHAR(20) NOT NULL,
 
-	UNIQUE (SequenceID),
     PRIMARY KEY (SequenceID),
 	FOREIGN KEY (OrderID) REFERENCES Orders(OrderID), 
     FOREIGN KEY (ProductID) REFERENCES Product(ProductID) 
